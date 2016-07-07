e1a6772e8a24b960381857d9ad0fe862abbb0ca2
ChocolateAceCream <nuodi@hotmail.com>
2016-07-07 14:44:41 -0400

    finish model test

---
+++ myapp/spec/factories/contacts.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :contact do
+    full_name     { Faker::Name.name }
+    email         { Faker::Internet.email }
+    phone_number  { Faker::PhoneNumber.phone_number }
+    address       { Faker::Address.street_address }
+  end
+end
---
+++ myapp/spec/models/contact_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+
+	it "has a valid factory" do
+		expect(FactoryGirl.create(:contact)).to be_valid
+	end
+
+	it "is invalid without a firstname" do
+		expect(FactoryGirl.build(:contact, full_name: nil)).not_to be_valid
+	end
+
+	 it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:phone_number) }
+    it { is_expected.to validate_presence_of(:address) }
+
+end
